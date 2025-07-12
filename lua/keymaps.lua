13ef0839c04bcd6f4674935868fcd19bb28c773c
LasseBloch <blochbuster@gmail.com>
2025-07-12 20:57:44 +0200

    small steps, Folke knows best

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -37,6 +37,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Use jk as escape
+vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Use jk as escape' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
